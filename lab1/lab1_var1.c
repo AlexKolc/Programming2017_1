93f2a4e18b2b794b9ff41407b8638e6acf3f0caa
alexkolc <alex_kolc@inbox.ru>
2017-09-18 00:43:26 +0300

    first commit

---
+++ lab1/lab1_var1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char s[50];
+    printf("Input string: ");
+    scanf("%s", s);
+    printf("Output string: %s\n", s);
+    int a;
+    printf("Input number: ");
+    scanf("%d", &a);
+    printf("Output number: %d", a);
+    return 1;
+}
