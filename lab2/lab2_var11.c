3c831d36d7fa69361b4046912a867b096f95a54c
AlexKolc <alex_kolc@inbox.ru>
2017-09-20 01:35:39 +0300

    Update lab2_var11.c

--- lab2/lab2_var11.c
+++ lab2/lab2_var11.c
@@ -5,7 +5,7 @@ int main() {
     float alpha;
     printf("Input alpha: ");
     scanf("%f", &alpha);
-    float z1 =  (1 - 2*pow(sin(alpha), 2.))/(1 + sin(2 * alpha));
+    float z1 =  (1 - 2*pow(sin(alpha), 2.))/(1 + sin(2*alpha));
     float z2 = (1 - tan(alpha))/(1 + tan(alpha));
     printf("z1 = %f\nz2 = %f", z1, z2);
     return 0;
